650f8b5f702f3ec7932f93a355d2cb8d160fa4ad
MIhaela Mamic <mihaela.mamic1@gmail.com>
2023-11-07 12:21:16 +0000

    /

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,7 +1,14 @@
-select
+with 
+source as (
+    select * from mmamic.orders
+),
+staged as (
+    select
     id as order_id,
     user_id as customer_id,
     order_date,
     status
+    from source
+)
 
-from mmamic.orders
+select * from staged
