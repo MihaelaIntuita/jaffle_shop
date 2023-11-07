8d9a1639c9e033152c17a5997a24fe4343835017
MIhaela Mamic <mihaela.mamic1@gmail.com>
2023-11-07 11:43:06 +0000

    /

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,9 +1,30 @@
-WITH fct_orders AS (
-  SELECT
-    orderid,
-    customer_id,
-    amount
-  FROM {{ ref('stg_payments') }}
+with orders as  (
+    select * from {{ ref('stg_orders' )}}
+),
+
+payments as (
+    select * from {{ ref('stg_payments') }}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+
+    from payments
+    group by 1
+),
+
+final as (
+
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payments.amount, 0) as amount
+
+    from orders
+    left join order_payments using (order_id)
 )
-SELECT *
-FROM fct_orders
+
+select * from final
--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -1,13 +1,13 @@
 WITH stg_payments AS (
-  SELECT
-    id as customer_id,
-    orderid,
-    paymentmethod,
-    status,
-    amount,
-    created,
-    _batched_at
-  FROM mmamic.payment
+    SELECT
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        amount / 100 as amount,  
+        created as created_at,
+        _batched_at
+    FROM mmamic.payment
 )
-
 SELECT * FROM stg_payments
+
