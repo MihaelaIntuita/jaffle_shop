9e2b8fa19b62b68db53fb3bc8f507ae2c3b77bcf
MIhaela Mamic <mihaela.mamic1@gmail.com>
2023-11-08 08:48:41 +0000

    /

--- models/staging/stg_payments.sql
+++ models/staging/stg_payments.sql
@@ -8,6 +8,7 @@ staged as(
         orderid as order_id,
         paymentmethod as payment_method,
         status,
+        -- amount is stored in cents, convert it to dollars
         amount / 100 as amount,  
         created as created_at,
         _batched_at
